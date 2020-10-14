63a582a521a501c23a693b8f1ae85678fa08f513
Adrian Ochmann <adrian.ochmann@outlook.com>
2020-10-14 10:10:35 +0200

    Simple refactor public access api controller

--- uCondition.ConditionalPublicAccess/uConditionPublicAccessApiController.cs
+++ uCondition.ConditionalPublicAccess/uConditionPublicAccessApiController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Script.Serialization;
@@ -11,7 +10,7 @@ using Umbraco.Web.WebApi;
 namespace uCondition.ConditionalPublicAccess
 {
     [IsBackOffice]
-    public class uConditionPublicAccessApiController : UmbracoApiController
+    public class UConditionPublicAccessApiController : UmbracoApiController
     {
         [UmbracoAuthorize]
         [HttpPost]
@@ -22,19 +21,21 @@ namespace uCondition.ConditionalPublicAccess
             var scriptSerializer = new JavaScriptSerializer();
             var protectedPageConditionList = new List<ProtectedPageCondition>();
 
-            foreach (ProtectedPageConditionViewModel condition in model.Conditions)
+            foreach (var condition in model.Conditions)
             {
-                ProtectedPageCondition protectedPageCondition = new ProtectedPageCondition()
+                var protectedPageCondition = new ProtectedPageCondition()
                 {
                     Condition = scriptSerializer.Serialize(condition.Condition),
                     FalseActionNodeId = condition.FalseActionNodeId
                 };
+
                 protectedPageConditionList.Add(protectedPageCondition);
             }
 
             if (protectedPages.Pages.Any(c => c.Id == id))
             {
                 var protectedPage = protectedPages.Pages.FirstOrDefault(p => p.Id == id);
+
                 protectedPage.Conditions = protectedPageConditionList;
                 protectedPage.Condition = scriptSerializer.Serialize((object)model.Condition);
                 protectedPage.FalseActionNodeId = model.FalseActionNodeId;
@@ -43,12 +44,14 @@ namespace uCondition.ConditionalPublicAccess
             {
                 var pages = protectedPages.Pages;
                 var protectedPage = new ProtectedPage();
+
                 protectedPage.Id = id;
-                protectedPage.Condition = scriptSerializer.Serialize((object)model.Condition);
+                protectedPage.Condition = scriptSerializer.Serialize(model.Condition);
                 protectedPage.FalseActionNodeId = model.FalseActionNodeId;
                 protectedPage.Conditions = protectedPageConditionList;
                 pages.Add(protectedPage);
             }
+
             protectedPageProvider.Save(protectedPages);
         }
 
@@ -57,22 +60,29 @@ namespace uCondition.ConditionalPublicAccess
         public ProtectedPageViewModel Get([FromUri] int id)
         {
             var protectedPages = new ProtectedPageProvider().Load();
-            var protectedPage = protectedPages != null ? protectedPages.Pages.FirstOrDefault(p => p.Id == id) : null;
+            var protectedPage = protectedPages?.Pages.FirstOrDefault(p => p.Id == id);
+
             if (protectedPage == null)
+            {
                 return null;
+            }
 
             var protectedPageViewModel = new ProtectedPageViewModel();
-            foreach (ProtectedPageCondition condition in protectedPage.Conditions)
+
+            foreach (var condition in protectedPage.Conditions)
             {
                 var conditionViewModel = new ProtectedPageConditionViewModel()
                 {
                     Condition = new uConditionModelConverter().Convert(condition.Condition),
                     FalseActionNodeId = condition.FalseActionNodeId
                 };
+
                 protectedPageViewModel.Conditions.Add(conditionViewModel);
             }
+
             protectedPageViewModel.Condition = new uConditionModelConverter().Convert(protectedPage.Condition);
             protectedPageViewModel.FalseActionNodeId = protectedPage.FalseActionNodeId;
+
             return protectedPageViewModel;
         }
 
@@ -83,6 +93,7 @@ namespace uCondition.ConditionalPublicAccess
             var protectedPageProvider = new ProtectedPageProvider();
             var protectedPages = protectedPageProvider.Load();
             var protectedPage = protectedPages.Pages.FirstOrDefault(c => c.Id == id);
+
             protectedPages.Pages.Remove(protectedPage);
             protectedPageProvider.Save(protectedPages);
         }
